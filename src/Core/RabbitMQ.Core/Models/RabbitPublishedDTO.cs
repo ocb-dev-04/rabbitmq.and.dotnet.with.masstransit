26c5b9c92f029a541729af1b6ef70f4a6aa9cd26
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-06-28 22:25:39 -0400

    *Add messageBus to controllers

--- src/Core/RabbitMQ.Core/Models/RabbitPublishedDTO.cs
+++ src/Core/RabbitMQ.Core/Models/RabbitPublishedDTO.cs
@@ -2,7 +2,7 @@
 
 public sealed class RabbitPublishedDTO
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Event { get; set; }
 }
